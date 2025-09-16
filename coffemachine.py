6a491c17a121474509f8bb6d8ea718447116b63a
Felix Barsch <mail@felixbarsch.com>
2025-09-16 10:42:02 +0200

    Funktion weggenommen.

--- coffemachine.py
+++ coffemachine.py
@@ -9,17 +9,17 @@ print("---------------------------------------")
 auswahl = int(input("Bitte Wählen Sie eine Option:"))
 price = 0
 
-def menueauswahl(auswahl):
-    match auswahl:
-        case 1 : ##Latte Machiato
+
+match auswahl:
+    case 1 : ##Latte Machiato
             price = 5.00
             print("Der Latte Machiato kostet: " , price)
         
-        case 2 : ##Espresso
+    case 2 : ##Espresso
             price = 3.00
             print("Der Espresso kostet: " , price)
 
-        case 3 : ##Cappuccino
+    case 3 : ##Cappuccino
             price = 4.50
             print("Der Cappuccino kostet: " , price)
 
