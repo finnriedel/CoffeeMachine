f792c8ccd8e70692fc22e7a41a7f32c462ba648b
Felix Barsch <mail@felixbarsch.com>
2025-09-16 11:36:00 +0200

    Korrektur der Menüführung nach Vorstellung.

--- coffemachine.py
+++ coffemachine.py
@@ -1,29 +1,30 @@
 auswahl = 0
-while auswahl != 4:
+while auswahl != "off":
 
     print("---------------------------------------")
     print("Folgende Getränke stehen zur Verfügung:")
     print("1: Latte Macchiato")
     print("2: Espresso")
     print("3: Cappuchino")
-    print("4: [AUSSCHALTEN]")
+    print("off: [AUSSCHALTEN]")
     print("---------------------------------------")
 
-    auswahl = int(input("Bitte Wählen Sie eine Option: "))
+    auswahl = input("Bitte Wählen Sie eine Option: ")
     price = 0
 
 
     match auswahl:
-        case 1 : ##Latte Machiato
+        case "1" : ##Latte Machiato
             price = 5.00
             print("Der Latte Machiato kostet: ",price,"€")
+            print("Ihr Getränk wird zubereitet.")
 
-        case 2 : ##Espresso
+        case "2" : ##Espresso
             price = 3.00
             print("Der Espresso kostet: ",price,"€")
+            print("Ihr Getränk wird zubereitet.")
 
-        case 3 : ##Cappuccino
+        case "3" : ##Cappuccino
             price = 4.50
             print("Der Cappuccino kostet: ",price,"€")
-
-    print("Ihr Getränk wird zubereitet.")
+            print("Ihr Getränk wird zubereitet.")
