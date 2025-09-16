0006cfbadc4e4206e4db4f107addd143e4fc74c2
Finn Riedel <fr@finnriedel.com>
2025-09-16 11:46:10 +0200

    Kaffebohnen hinzugefügt

--- coffemachine.py
+++ coffemachine.py
@@ -1,13 +1,14 @@
 auswahl = 0
-while auswahl != "off":
+kaffebohnen = 10
+while auswahl != "off" and kaffebohnen != 0:
 
-    print("---------------------------------------")
-    print("Folgende Getränke stehen zur Verfügung:")
-    print("1: Latte Macchiato")
-    print("2: Espresso")
-    print("3: Cappuchino")
-    print("off: [AUSSCHALTEN]")
-    print("---------------------------------------")
+    print("-------------------------------------------")
+    print("| Folgende Getränke stehen zur Verfügung: |")
+    print("| 1: Latte Macchiato                      |")
+    print("| 2: Espresso                             |")
+    print("| 3: Cappuchino                           |")
+    print("| off: [AUSSCHALTEN]                      |")
+    print("-------------------------------------------")
 
     auswahl = input("Bitte Wählen Sie eine Option: ")
     price = 0
@@ -30,8 +31,15 @@ while auswahl != "off":
             print("Die Maschine wird ausgeschaltet...")
             break
         
+        case "report":
+            print("Report")
+
+        case "replenish":
+            print("Auffüllen")
+
         case _:
             print("Fehlerhafte Eingabe.")
             break
 
+    kaffebohnen = kaffebohnen-1
     print("Ihr Getränk wird zubereitet.")
