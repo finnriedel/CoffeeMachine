d9037582b06d18db396de5fa6f28af229ddd343a
Liam Culmey <liam.culmey@web.de>
2025-09-16 10:43:16 +0200

    Eurozeichen beim Preis.

--- coffemachine.py
+++ coffemachine.py
@@ -13,13 +13,13 @@ def menueauswahl(auswahl):
     match auswahl:
         case 1 : ##Latte Machiato
             price = 5.00
-            print("Der Latte Machiato kostet: " , price)
+            print("Der Latte Machiato kostet: " , price,"€")
         
         case 2 : ##Espresso
             price = 3.00
-            print("Der Espresso kostet: " , price)
+            print("Der Espresso kostet: " , price,"€")
 
         case 3 : ##Cappuccino
             price = 4.50
-            print("Der Cappuccino kostet: " , price)
+            print("Der Cappuccino kostet: " , price,"€")
 
