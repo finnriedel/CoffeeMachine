3aa611679e502e31dfe394bad2a66239f5357120
Felix Barsch <mail@felixbarsch.com>
2025-09-16 10:39:13 +0200

    Korrektur Klammer

--- coffemachine.py
+++ coffemachine.py
@@ -3,7 +3,7 @@ print("Folgende Getränke stehen zur Verfügung:")
 print("1: Latte Macchiato")
 print("2: Espresso")
 print("3: Cappuchino")
-print("4: [AUSSCHALTEN]
+print("4: [AUSSCHALTEN])
 print("---------------------------------------")
 
 auswahl = input(int("Bitte Wählen Sie eine Option:"))
