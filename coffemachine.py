396acd1a829d1e5ea23e5c60c6deb0889f58f254
Finn Riedel <fr@finnriedel.com>
2025-09-16 10:36:22 +0200

    Kaffe Angebot eingestellt

--- coffemachine.py
+++ coffemachine.py
@@ -1,4 +1,10 @@
-
+print("---------------------------------------")
+print("Folgende Getränke stehen zur Verfügung:")
+print("1: Latte Macchiato")
+print("2: Espresso")
+print("3: Cappuchino")
+print("4: [AUSSCHALTEN]
+print("---------------------------------------")
 
 auswahl = input(int("Bitte Wählen Sie eine Option:"))
 
