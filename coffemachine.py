dc2c406549cd6c7ae645f5f8009f746afebaa4d1
Felix Barsch <mail@felixbarsch.com>
2025-09-16 10:45:15 +0200

    Leerzeichen-Korrektur

--- coffemachine.py
+++ coffemachine.py
@@ -6,20 +6,20 @@ print("3: Cappuchino")
 print("4: [AUSSCHALTEN]")
 print("---------------------------------------")
 
-auswahl = int(input("Bitte Wählen Sie eine Option:"))
+auswahl = int(input("Bitte Wählen Sie eine Option: "))
 price = 0
 
 
 match auswahl:
     case 1 : ##Latte Machiato
             price = 5.00
-            print("Der Latte Machiato kostet: " , price,"€")
+            print("Der Latte Machiato kostet: ",price,"€")
         
     case 2 : ##Espresso
             price = 3.00
-            print("Der Espresso kostet: " , price,"€")
+            print("Der Espresso kostet: ",price,"€")
 
     case 3 : ##Cappuccino
             price = 4.50
-            print("Der Cappuccino kostet: " , price,"€")
+            print("Der Cappuccino kostet: ",price,"€")
 
