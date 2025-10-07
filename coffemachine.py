48277b51592bd20577d5a66669c02330af9b8af5
Finn Riedel <fr@finnriedel.com>
2025-10-07 11:16:24 +0200

    While-Schleife mit off

--- coffemachine.py
+++ coffemachine.py
@@ -1,53 +1,57 @@
-menue = {
-    "Latte Machiato":{
-        "price":5,
-        "water":"100ml",
-        "Coffee":"25g"
-    }, 
-    "Espresso":{
-        "price":3,
-        "water":"50ml",
-        "coffe":"25g",
-        "milk":"100ml Oatmilk"
-         }, 
-    "Cappuccino":{
-        "price":4.5,
-        "water":"250ml",
-        "coffee":"25g",
-        "milk":"250ml Oatmilk"
-         }}
+while True: 
+    menue = {
+        "Latte Machiato":{
+            "price":5,
+            "water":"100ml",
+            "Coffee":"25g"
+        }, 
+        "Espresso":{
+            "price":3,
+            "water":"50ml",
+            "coffe":"25g",
+            "milk":"100ml Oatmilk"
+            }, 
+        "Cappuccino":{
+            "price":4.5,
+            "water":"250ml",
+            "coffee":"25g",
+            "milk":"250ml Oatmilk"
+            }}
 
-for i in menue:
-    print(i)
+    for i in menue:
+        print(i)
 
-auswahl = input("Kaffee auswählen: ")
+    auswahl = input("Kaffee auswählen: ")
 
-if auswahl in menue:
-    preis = menue[auswahl]['price']
-    print("Der ausgewählte Kaffe kostet:", preis, "€")
-else:
-    print("Ungültige Auswahl")
+    if auswahl in menue:
+        preis = menue[auswahl]['price']
+        print("Der ausgewählte Kaffe kostet:", preis, "€")
+    elif auswahl == "off":
+        print("Kaffemaschine wird ausgeschaltet...")
+        break
+    else:
+        print("Ungültige Auswahl")
 
-while(preis > 0):
-    muenzeinwurf = float(input("Münzen einwerfen: "))
+    while(preis > 0):
+        muenzeinwurf = float(input("Münzen einwerfen: "))
 
-    match muenzeinwurf:
-        case 0.1:
-            preis=preis-0.1
-        case 0.2:
-            preis=preis-0.2
-        case 0.5:
-            preis=preis-0.5
-        case 1:
-            preis=preis-1
-        case 2:
-            preis=preis-2
-        case _:
-            print("Bitte werfen Sie nur gültige Geldstücke ein")
-    if(preis > 0):        
-        print("Es fehlen noch:", preis,"€")
+        match muenzeinwurf:
+            case 0.1:
+                preis=preis-0.1
+            case 0.2:
+                preis=preis-0.2
+            case 0.5:
+                preis=preis-0.5
+            case 1:
+                preis=preis-1
+            case 2:
+                preis=preis-2
+            case _:
+                print("Bitte werfen Sie nur gültige Geldstücke ein")
+        if(preis > 0):        
+            print("Es fehlen noch:", preis,"€")
 
-if(preis < 0):
-    print("Sie bekommen", abs(preis), "€ Rückgeld")
+    if(preis < 0):
+        print("Sie bekommen", abs(preis), "€ Rückgeld")
 
-print("Kaffe wird ausgegeben")
+    print("Kaffe wird ausgegeben")
