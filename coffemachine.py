fa6f3091468b0c15a76adf318eff0512131b43ba
Finn Riedel <fr@finnriedel.com>
2025-10-07 09:59:44 +0200

    Added stats to dict

--- coffemachine.py
+++ coffemachine.py
@@ -1,12 +1,20 @@
 menue = {
     "Latte Machiato":{
-        "Price":5
+        "Price":5,
+        "Water":"100ml",
+        "Coffee":"25g"
     }, 
     "Espresso":{
-        "Price":3
+        "Price":3,
+        "Water":"50ml",
+        "Coffe":"25g",
+        "Oatmilk":"100ml Oatmilk"
          }, 
     "Cappuccino":{
-        "Price":4.5
+        "Price":4.5,
+        "Water":"250ml",
+        "Coffee":"25g",
+        "Milk":"250ml Oatmilk"
          }}
 
 
