f816460529550e705abdf120d346711ad57ec27c
Finn Riedel <fr@finnriedel.com>
2025-11-04 14:42:08 +0100

    Cash_drawer implementiert

--- coffemachine.py
+++ coffemachine.py
@@ -6,7 +6,39 @@
 water_tank = 2000
 coffee_grinder = 50
 milk_tank = 1000
-cash_drawer = 0
+
+cash_drawer = {
+    "0,10€":{
+        "wert": 0.1,
+        "muenzen": 5
+    },
+    "0,20€":{
+        "wert": 0.2,
+        "muenzen": 5
+    },
+    "0,50€":{
+        "wert": 0.5,
+        "muenzen": 5
+    },
+    "1,00€":{
+        "wert": 1.0,
+        "muenzen": 5
+    },
+    "2,00€":{
+        "wert": 2.0,
+        "muenzen": 5
+    }
+}
+
+def cash_drawer_summieren():
+    summe = 0
+    for element in cash_drawer:
+        summe = summe + (cash_drawer[element]['wert'] * cash_drawer[element]['muenzen'])
+    return summe
+
+def cash_drawer_balance():
+    for element in cash_drawer:
+        print(cash_drawer[element], ":", cash_drawer[element]['muenzen'])
 
 while True:
 
@@ -50,6 +82,9 @@ while True:
     elif auswahl == "off":
         print("Kaffemaschine wird ausgeschaltet...")
         break
+    elif auswahl == "balance":
+        cash_drawer_balance()
+        continue
     else:
         print("Ungültige Auswahl")
         continue
@@ -63,14 +98,19 @@ while True:
                 break
             case 0.1:
                 preis=preis-0.1
+                cash_drawer["0,10€"]['muenzen']+1
             case 0.2:
                 preis=preis-0.2
+                cash_drawer["0,20€"]['muenzen']+1
             case 0.5:
                 preis=preis-0.5
+                cash_drawer["0,50€"]['muenzen']+1
             case 1:
                 preis=preis-1
+                cash_drawer["1,00€"]['muenzen']+1
             case 2:
                 preis=preis-2
+                cash_drawer["2,00€"]['muenzen']+1
             case _:
                 print("Bitte werfen Sie nur gültige Geldstücke ein")
         if(preis > 0):        
@@ -80,27 +120,35 @@ while True:
     water_tank = water_tank-menue[auswahl]['water']
     coffee_grinder = coffee_grinder-menue[auswahl]['coffee']
     milk_tank = milk_tank-coffee_grinder-menue[auswahl]['milk']
-    cash_drawer = cash_drawer+menue[auswahl]['price']
 
-    if(preis < 0):
+    if(preis < 0 and cash_drawer_summieren() >= abs(preis)):
         print("Sie bekommen", abs(preis), "€ Rückgeld")
-        cash_drawer = cash_drawer - abs(preis)
 
         while(preis != 0):
             if(preis <= -2):
-                preis=preis+2
-                print("2,00€")
+                if cash_drawer["2,00€"]['muenzen'] > 0:
+                    preis=preis+2
+                    cash_drawer["2,00€"]['muenzen']-1
+                    print("2,00€")
             elif(preis <= -1):
-                preis=preis+1
-                print("1,00€")
+                if cash_drawer["1,00€"]['muenzen'] > 0:
+                    preis=preis+1
+                    cash_drawer["1,00€"]['muenzen']-1
+                    print("1,00€")
             elif(preis <= -0.5):
-                preis=preis+0.5
-                print("0,50€")
+                if cash_drawer["0,50€"]['muenzen'] > 0:
+                    preis=preis+0.5
+                    cash_drawer["0,50€"]['muenzen']-1
+                    print("0,50€")
             elif(preis <= -0.2):
-                preis=preis+0.2
-                print("0,20€")
+                if cash_drawer["0,20€"]['muenzen'] > 0:
+                    cash_drawer["0,20€"]['muenzen']-1
+                    preis=preis+0.2
+                    print("0,20€")
             elif(preis <= -0.1):
-                preis=preis+0.1
-                print("0,10€")
+                if cash_drawer["0,10€"]['muenzen'] > 0:
+                    preis=preis+0.1
+                    cash_drawer["0,10€"]['muenzen']-1
+                    print("0,10€")
 
     print("Kaffe wird ausgegeben")
