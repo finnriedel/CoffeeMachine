1290aebe3f43f0ec55a8ee2b03bf33a3122a810e
Finn Riedel <fr@finnriedel.com>
2025-11-18 13:17:17 +0100

    c
    x

--- counting_sort.py
+++ counting_sort.py
@@ -1,1 +1,18 @@
-test
+arr = [44,55,12,42,94,18,6,67,6]
+count_arr = [0]*(max(arr)+1)
+
+for i in range(len(arr)):
+    index = arr[i]
+    count_arr[index] = count_arr[index]+1
+
+print("Counting-Array:",count_arr)
+
+print("\nFertiges Array (Aufsteigend):")
+for i in range(len(count_arr)):
+    if(count_arr[i] > 0):
+        print(i,end=", ")
+
+print("\n\nFertiges Array (Absteigend):")
+for i in range(len(count_arr)-1, 0, -1):
+    if(count_arr[i] > 0):
+        print(i, end=", ")
